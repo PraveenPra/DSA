ce36ad1a6dffd54dbdbe23216d2422a7a730eb4e
PraveenPra <prvp6501@gmail.com>
2023-10-11 01:22:43 +0530

    Implemented the logic for array sort

--- ArraySort/main.js
+++ ArraySort/main.js
@@ -12,9 +12,21 @@ function display(dataInfo, container) {
 
 }
 
+// loops iterate array size x array size. here 4*4 = 16 times, so we taken 2 for loops to implement this
 function sortArray() {
 
-   
+    //main loop for each element iteration
+   for(i=0; i<data.length; i++){
+
+    //perform sorting/ switching here
+    for(j=0;j<data.length; j++){
+        if(data[j]>data[j+1]){
+            let temp = data[j];
+            data[j] = data[j+1];
+            data[j+1] = temp
+        }
+    }
+   }
     display(data, output);
 
     data = [60, 30, 10, 67, 40];
